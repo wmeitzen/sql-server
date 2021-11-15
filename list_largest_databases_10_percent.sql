3828597e44d1e954e90eb78147ce74f7ab68c84b
William Meitzen <wmeitzen@gmail.com>
2021-11-15 12:11:27 -0500

    initial commit

---
+++ list_largest_databases_10_percent.sql
@@ -0,0 +1,6 @@
+SELECT top 10 percent D.name, CONVERT(DECIMAL(10,2)
+,sum((F.size * 8.00) / 1024.00 / 1024.00)) As UsedSpace_GB
+FROM master.sys.master_files as F
+inner join sys.databases as D on F.database_id = D.database_id
+group by D.name
+order by 2 desc
