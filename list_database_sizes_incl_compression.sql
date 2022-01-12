ecb93603c141795818eee18235f0c8bd047e51b7
William Meitzen <wmeitzen@gmail.com>
2022-01-12 11:57:18 -0500

    added MB values

--- list_database_sizes_incl_compression.sql
+++ list_database_sizes_incl_compression.sql
@@ -1,9 +1,11 @@
+
 declare @fltCompressionMultiplier float = 0.5
 
 SELECT /*top 10 percent*/
 D.name
 , @@version as [@@version]
 ,CONVERT(DECIMAL(10,2),sum((F.size * 8.00) / 1024.00 / 1024.00)) As UsedSpace_GB
+,CONVERT(DECIMAL(10,2),sum((F.size * 8.00) / 1024.00)) As UsedSpace_MB
 ,case
 	when @@version like '%2022%' then 1
 	when @@version like '%2019%' then 1
@@ -21,6 +23,15 @@ end as compression_available
 		when @@version like '%enterprise%' then @fltCompressionMultiplier
 		else 1
 	end) as approximate_compression_GB
+,CONVERT(DECIMAL(10,2),sum((F.size * 8.00) / 1024.00)
+	* case
+		when @@version like '%2022%' then @fltCompressionMultiplier
+		when @@version like '%2019%' then @fltCompressionMultiplier
+		when @@version like '%2017%' then @fltCompressionMultiplier
+		when @@version like '%2016%' and @@version like '%sp2%' then @fltCompressionMultiplier
+		when @@version like '%enterprise%' then @fltCompressionMultiplier
+		else 1
+	end) as approximate_compression_MB
 FROM master.sys.master_files as F
 inner join sys.databases as D on F.database_id = D.database_id
 where D.name not in ('tempdb')
