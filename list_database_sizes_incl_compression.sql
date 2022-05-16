2fd2473872eb47f796039a1afb9bcf3f8ec01cb1
William Meitzen <wmeitzen@gmail.com>
2022-05-16 17:45:09 -0400

    corrected version number syntax issues

--- list_database_sizes_incl_compression.sql
+++ list_database_sizes_incl_compression.sql
@@ -1,4 +1,3 @@
-
 declare @fltCompressionMultiplier float
 set @fltCompressionMultiplier = 0.5
 
@@ -8,28 +7,31 @@ D.name
 ,CONVERT(DECIMAL(10,2),sum((F.size * 8.00) / 1024.00 / 1024.00)) As UsedSpace_GB
 ,CONVERT(DECIMAL(10,2),sum((F.size * 8.00) / 1024.00)) As UsedSpace_MB
 ,case
-	when @@version like '%2022%' then 1
-	when @@version like '%2019%' then 1
-	when @@version like '%2017%' then 1
-	when @@version like '%2016%' and @@version like '%sp2%' then 1
+	when @@version like '%Server 2022%' then 1
+	when @@version like '%Server 2019%' then 1
+	when @@version like '%Server 2017%' then 1
+	when @@version like '%Server 2016%' and @@version like '%sp2%' then 1
+	when @@version like '%Server 2005%' then 0
 	when @@version like '%enterprise%' then 1
 	else 0
 end as compression_available
 ,CONVERT(DECIMAL(10,2),sum((F.size * 8.00) / 1024.00 / 1024.00)
 	* case
-		when @@version like '%2022%' then @fltCompressionMultiplier
-		when @@version like '%2019%' then @fltCompressionMultiplier
-		when @@version like '%2017%' then @fltCompressionMultiplier
-		when @@version like '%2016%' and @@version like '%sp2%' then @fltCompressionMultiplier
+		when @@version like '%Server 2022%' then @fltCompressionMultiplier
+		when @@version like '%Server 2019%' then @fltCompressionMultiplier
+		when @@version like '%Server 2017%' then @fltCompressionMultiplier
+		when @@version like '%Server 2016%' and @@version like '%sp2%' then @fltCompressionMultiplier
+		when @@version like '%Server 2005%' then 1
 		when @@version like '%enterprise%' then @fltCompressionMultiplier
 		else 1
 	end) as approximate_compression_GB
 ,CONVERT(DECIMAL(10,2),sum((F.size * 8.00) / 1024.00)
 	* case
-		when @@version like '%2022%' then @fltCompressionMultiplier
-		when @@version like '%2019%' then @fltCompressionMultiplier
-		when @@version like '%2017%' then @fltCompressionMultiplier
-		when @@version like '%2016%' and @@version like '%sp2%' then @fltCompressionMultiplier
+		when @@version like '%Server 2022%' then @fltCompressionMultiplier
+		when @@version like '%Server 2019%' then @fltCompressionMultiplier
+		when @@version like '%Server 2017%' then @fltCompressionMultiplier
+		when @@version like '%Server 2016%' and @@version like '%sp2%' then @fltCompressionMultiplier
+		when @@version like '%Server 2005%' then 1
 		when @@version like '%enterprise%' then @fltCompressionMultiplier
 		else 1
 	end) as approximate_compression_MB
