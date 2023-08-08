86f2880d71b2e24824dd5743b5d30290e2b88707
William Meitzen <wmeitzen@gmail.com>
2023-08-08 09:57:20 -0400

    changed top 10% to 50%

--- list_largest_databases_10_percent.sql
+++ list_largest_databases_10_percent.sql
@@ -1,4 +1,4 @@
-SELECT top 10 percent D.name, CONVERT(DECIMAL(10,2)
+SELECT top 50 percent D.name, CONVERT(DECIMAL(10,2)
 ,sum((F.size * 8.00) / 1024.00 / 1024.00)) As UsedSpace_GB
 FROM master.sys.master_files as F
 inner join sys.databases as D on F.database_id = D.database_id
