17f7a6a6507f7f25ea9b721ddecf76e2a2065a3e
William Meitzen <wmeitzen@gmail.com>
2024-09-04 16:29:19 -0500

    85% or 8GB reserved

--- set-max-memory.sql
+++ set-max-memory.sql
@@ -1,21 +1,47 @@
-
 set nocount on
 go
 
+declare @intLeaveMBForOSMin bigint = 4 *1024
+declare @intLeaveMBForOSMax bigint = 8 *1024
 declare @intTotalPhysicalMemoryMB bigint
-declare @int85PercentTotalPhysicalMemoryMB bigint
+declare @intPercentage int = 85
+declare @intPercentTotalPhysicalMemoryMB bigint
 declare @intMaxServerMemorySettingMB bigint
+declare @intRecommendedOSMemoryMB bigint
 
 SELECT @intTotalPhysicalMemoryMB = [total_physical_memory_kb] / 1024 FROM [master].[sys].[dm_os_sys_memory]
-SELECT @int85PercentTotalPhysicalMemoryMB = ([total_physical_memory_kb] / 1024) * 0.85 FROM [master].[sys].[dm_os_sys_memory]
+SELECT @intPercentTotalPhysicalMemoryMB = ([total_physical_memory_kb] / 1024) * (@intPercentage / 100.0) FROM [master].[sys].[dm_os_sys_memory]
 SELECT @intMaxServerMemorySettingMB = cast(value_in_use as bigint) FROM sys.configurations where name = 'max server memory (MB)'
+set @intRecommendedOSMemoryMB = @intTotalPhysicalMemoryMB - @intPercentTotalPhysicalMemoryMB
 
 print 'Server name: ' + @@servername
+print 'Current Max Server Memory setting = ' + cast(@intMaxServerMemorySettingMB as varchar(15)) + ' MB'
 print 'Total physical memory = ' + cast(@intTotalPhysicalMemoryMB as varchar(15)) + ' MB'
-print '85% of total physical memory = ' + cast(@int85PercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
-print 'Max Server Memory setting = ' + cast(@intMaxServerMemorySettingMB as varchar(15)) + ' MB'
+print cast(@intPercentage as varchar(2)) + '% of total physical memory = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
+print 'Recommended Max Server Memory setting = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
+print 'Remaining memory for OS (recommended) = ' + cast(@intRecommendedOSMemoryMB as varchar(6)) + ' MB'
 
-if @intMaxServerMemorySettingMB between @int85PercentTotalPhysicalMemoryMB * 0.95 and @int85PercentTotalPhysicalMemoryMB * 1.05
+if @intRecommendedOSMemoryMB < @intLeaveMBForOSMin
+begin
+	set @intRecommendedOSMemoryMB = @intLeaveMBForOSMin
+	set @intPercentTotalPhysicalMemoryMB = @intTotalPhysicalMemoryMB - @intRecommendedOSMemoryMB
+	print 'Remaining memory for OS is too low (less than ' + cast(@intLeaveMBForOSMin as varchar(6)) + ' MB)'
+	print 'Corrected:'
+	print 'Recommended Max Server Memory setting = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
+	print 'Remaining memory for OS = ' + cast(@intRecommendedOSMemoryMB as varchar(6)) + ' MB'
+end
+
+if @intRecommendedOSMemoryMB > @intLeaveMBForOSMax
+begin
+	set @intRecommendedOSMemoryMB = @intLeaveMBForOSMax
+	set @intPercentTotalPhysicalMemoryMB = @intTotalPhysicalMemoryMB - @intRecommendedOSMemoryMB
+	print 'Remaining memory for OS is excessive (more than ' + cast(@intLeaveMBForOSMax as varchar(6)) + ' MB)'
+	print 'Corrected:'
+	print 'Recommended Max Server Memory setting = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
+	print 'Remaining memory for OS = ' + cast(@intRecommendedOSMemoryMB as varchar(6)) + ' MB'
+end
+
+if @intMaxServerMemorySettingMB between @intPercentTotalPhysicalMemoryMB * 0.95 and @intPercentTotalPhysicalMemoryMB * 1.05
 begin
 	print 'Memory setting is ok'
 end
@@ -27,7 +53,7 @@ begin
 	print 'go'
 	print 'RECONFIGURE WITH OVERRIDE'
 	print 'go'
-	print 'EXEC sys.sp_configure N''max server memory (MB)'', N''' + cast(@int85PercentTotalPhysicalMemoryMB as varchar(15)) + ''' -- set to 85% of ' + cast(@intTotalPhysicalMemoryMB as varchar(15)) + ' MB, ' + cast(@int85PercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
+	print 'EXEC sys.sp_configure N''max server memory (MB)'', N''' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ''' -- RAM is ' + cast(@intTotalPhysicalMemoryMB as varchar(15)) + ' MB'
 	print 'go'
 	print 'RECONFIGURE WITH OVERRIDE'
 	print 'go'
