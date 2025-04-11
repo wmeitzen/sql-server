94c4d2938778c5dd002677b621ac6af6402d3370
William Meitzen <wmeitzen@gmail.com>
2025-04-10 21:22:45 -0500

    Updated for tiny amount of RAM (< 4GB)

--- set-max-memory.sql
+++ set-max-memory.sql
@@ -10,9 +10,15 @@ declare @intMaxServerMemorySettingMB bigint
 declare @intRecommendedOSMemoryMB bigint
 
 SELECT @intTotalPhysicalMemoryMB = [total_physical_memory_kb] / 1024 FROM [master].[sys].[dm_os_sys_memory]
-SELECT @intPercentTotalPhysicalMemoryMB = ([total_physical_memory_kb] / 1024) * (@intPercentage / 100.0) FROM [master].[sys].[dm_os_sys_memory]
+--set @intTotalPhysicalMemoryMB = 1000 -- for testing - delete later
+SELECT @intPercentTotalPhysicalMemoryMB = @intTotalPhysicalMemoryMB * (@intPercentage / 100.0)
 SELECT @intMaxServerMemorySettingMB = cast(value_in_use as bigint) FROM sys.configurations where name = 'max server memory (MB)'
+--print '@intTotalPhysicalMemoryMB = ' + cast(@intTotalPhysicalMemoryMB as varchar(32))
+--print '@intPercentTotalPhysicalMemoryMB = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(32))
 set @intRecommendedOSMemoryMB = @intTotalPhysicalMemoryMB - @intPercentTotalPhysicalMemoryMB
+--print '@intRecommendedOSMemoryMB = ' + cast(@intRecommendedOSMemoryMB as varchar(32))
+--print '@intLeaveMBForOSMin = ' + cast(@intLeaveMBForOSMin as varchar(32))
+--print '@intLeaveMBForOSMax = ' + cast(@intLeaveMBForOSMax as varchar(32))
 
 print 'Server name: ' + @@servername
 print 'Current Max Server Memory setting = ' + cast(@intMaxServerMemorySettingMB as varchar(15)) + ' MB'
@@ -21,24 +27,26 @@ print cast(@intPercentage as varchar(2)) + '% of total physical memory = ' + cas
 print 'Recommended Max Server Memory setting = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
 print 'Remaining memory for OS (recommended) = ' + cast(@intRecommendedOSMemoryMB as varchar(6)) + ' MB'
 
-if @intRecommendedOSMemoryMB < @intLeaveMBForOSMin
+if @intRecommendedOSMemoryMB < @intLeaveMBForOSMin and @intTotalPhysicalMemoryMB > @intLeaveMBForOSMin
 begin
 	set @intRecommendedOSMemoryMB = @intLeaveMBForOSMin
 	set @intPercentTotalPhysicalMemoryMB = @intTotalPhysicalMemoryMB - @intRecommendedOSMemoryMB
+--	print '(1)'
 	print 'Remaining memory for OS is too low (less than ' + cast(@intLeaveMBForOSMin as varchar(6)) + ' MB)'
-	print 'Corrected:'
-	print 'Recommended Max Server Memory setting = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
-	print 'Remaining memory for OS = ' + cast(@intRecommendedOSMemoryMB as varchar(6)) + ' MB'
+	print 'Adjusting:'
+	print 'Adjusting Max Server Memory setting to = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
+	print 'So remaining memory for OS = ' + cast(@intRecommendedOSMemoryMB as varchar(6)) + ' MB'
 end
 
-if @intRecommendedOSMemoryMB > @intLeaveMBForOSMax
+if @intRecommendedOSMemoryMB > @intLeaveMBForOSMax  -- 3600 > 
 begin
 	set @intRecommendedOSMemoryMB = @intLeaveMBForOSMax
 	set @intPercentTotalPhysicalMemoryMB = @intTotalPhysicalMemoryMB - @intRecommendedOSMemoryMB
+--	print '(2)'
 	print 'Remaining memory for OS is excessive (more than ' + cast(@intLeaveMBForOSMax as varchar(6)) + ' MB)'
-	print 'Corrected:'
-	print 'Recommended Max Server Memory setting = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
-	print 'Remaining memory for OS = ' + cast(@intRecommendedOSMemoryMB as varchar(6)) + ' MB'
+	print 'Adjusting:'
+	print 'Adjusting Max Server Memory setting to = ' + cast(@intPercentTotalPhysicalMemoryMB as varchar(15)) + ' MB'
+	print 'So remaining memory for OS = ' + cast(@intRecommendedOSMemoryMB as varchar(6)) + ' MB'
 end
 
 if @intMaxServerMemorySettingMB between @intPercentTotalPhysicalMemoryMB * 0.95 and @intPercentTotalPhysicalMemoryMB * 1.05
@@ -47,7 +55,7 @@ begin
 end
 else
 begin
-	print 'Memory setting is not ok. Commands:'
+	print 'Memory setting is not ideal. Commands:'
 	print ''
 	print 'EXEC sys.sp_configure N''show advanced options'', N''1'''
 	print 'go'
